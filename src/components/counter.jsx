f28c0a95d69f7631b67695cc4cee05af0a9a87aa
Madhura <madhura.bhujbal@gmail.com>
2020-07-22 22:46:47 +0100

    Wrap the code for changing button color in a seperate method instead of in render method and change the classname accordingly

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,17 +6,20 @@ class Counter extends Component {
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-
     return (
       <React.Fragment>
-        <span style={{ fontSize: 30 }} className={classes}>
+        <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
