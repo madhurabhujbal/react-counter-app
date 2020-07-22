5f9cca658d39faf7f95af7afe33056cfbba64e1f
Madhura <madhura.bhujbal@gmail.com>
2020-07-22 22:41:53 +0100

    Add code to change the button color depending on count value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,9 +6,12 @@ class Counter extends Component {
   };
 
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+
     return (
       <React.Fragment>
-        <span style={{ fontSize: 80 }} className="badge badge-primary m-2">
+        <span style={{ fontSize: 30 }} className={classes}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
