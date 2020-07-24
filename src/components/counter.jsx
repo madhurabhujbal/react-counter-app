d27e678fd7e063d2296578cc694f6df5d7d7992e
Madhura <madhura.bhujbal@gmail.com>
2020-07-24 08:21:43 +0100

    Add code to render list dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
@@ -12,6 +13,12 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {" "}
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}{" "}
+        </ul>
       </React.Fragment>
     );
   }
