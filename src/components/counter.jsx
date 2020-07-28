63de6921451fc0b81a56b150b8e1c8c40bc9d623
Madhura <madhura.bhujbal@gmail.com>
2020-07-28 22:37:19 +0100

    Delete the local state oject in counter component and replace state oject with props.counter everywhere in the code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    //state includes data that is local or private to a component
-    value: this.props.counter.value, //props include data that we give to a component. They are read-only and we cannot set values for props. They have to be set through state.
-  };
+  // state = {
+  //   //state includes data that is local or private to a component
+  //   value: this.props.counter.value, //props include data that we give to a component. They are read-only and we cannot set values for props. They have to be set through state.
+  // };
 
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this); //part after '=' will return an instance of handleIncrement method. Methods in javascript are objects; bind is built in method in js. handleIncrement method is reset to function returned from bind method.
   // }
 
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
+  // handleIncrement = (product) => {
+  //   console.log(product);
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   render() {
     return (
@@ -23,7 +23,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })} //For passing argument to event handler, define an arrow function, in body of function call the event handler, and pass argument to the event handler i.e product
+          onClick={() => this.props.onIncrement(this.props.counter)} //For passing argument to event handler, define an arrow function, in body of function call the event handler, and pass argument to the event handler i.e product
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -41,12 +41,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
