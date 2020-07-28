4c2c783f59ea1daba5b7341bb1e3567e3473f346
Madhura <madhura.bhujbal@gmail.com>
2020-07-28 12:09:35 +0100

    Add id property for displaying

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,9 +14,11 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            id={counter.id}
+          ></Counter>
         ))}
       </div>
     );
