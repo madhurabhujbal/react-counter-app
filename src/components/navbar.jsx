840690796932cb644d051c97a36a9545ca9134b4
Madhura <madhura.bhujbal@gmail.com>
2020-07-29 11:50:17 +0100

    Use object destructuring to destructure props and this.props argument and modify arguments accordingly

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 //Navbar component does not have any event handlers or helper methods for calculation; no state, as data is updated by refering to props. It just has a render method. In this case, we can convert Navbar component to Stateless Functional Component i.e instead of class, define a function.
 
-const Navbar = (props) => {
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
@@ -10,7 +10,7 @@ const Navbar = (props) => {
         <span className="badge badge-pill badge-secondary">
           {" "}
           {/* {this.props.totalCounters}{" "}  'this' works only in class. In SFC, we have to pass props as a parameter of arrow function and modify the reference accordingly*/}
-          {props.totalCounters}{" "}
+          {totalCounters}{" "}
         </span>
       </a>
     </nav>
