41eda15db9e21aecabd57b466b9cf4fbbd31e9e2
Madhura <madhura.bhujbal@gmail.com>
2020-07-29 11:35:26 +0100

    Convert navbar from class component to Stateless Functional Component; modify the reference to total counter on it accordingly

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {" "}
-            {this.props.totalCounters}{" "}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//Navbar component does not have any event handlers or helper methods for calculation; no state, as data is updated by refering to props. It just has a render method. In this case, we can convert Navbar component to Stateless Functional Component i.e instead of class, define a function.
+
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {" "}
+          {/* {this.props.totalCounters}{" "}  'this' works only in class. In SFC, we have to pass props as a parameter of arrow function and modify the reference accordingly*/}
+          {props.totalCounters}{" "}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
