d82a963f38b9f454f65de9ce0f77dafb125aba1a
Madhura <madhura.bhujbal@gmail.com>
2020-07-29 10:32:04 +0100

    Delete state and state mutation functions from counters component and add that code in App file

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,38 +2,6 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-    console.log(this.state.counters[index]);
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    //We cannot modify counters array in state object directly. Hence we are creating a new array which returns counters array except the one with counterId. We set the state of main counters array through this new array.
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <div>
